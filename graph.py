8f0b386678216a795ccbe335982c006788d4efa0
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-09 22:27:26 -0400

    reorder_routes_to_make_all_paths_lead_to_the_city_zero_1466

--- graph.py
+++ graph.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 def number_of_provinces_547_dfs(isConnected):
     """There are n cities. Some of them are connected, while some are not. If city a is connected directly with city b, and city b is connected directly with city c, then city a is connected indirectly with city c.
 
@@ -68,3 +70,67 @@ def number_of_provinces_547_bfs(isConnected):
             bfs(i)
 
     return provinces
+
+
+def reorder_routes_to_make_all_paths_lead_to_the_city_zero_1466(n, connections):
+    """There are n cities numbered from 0 to n - 1 and n - 1 roads such that there is only one way to travel between two different cities (this network form a tree). Last year, The ministry of transport decided to orient the roads in one direction because they are too narrow.
+
+    Roads are represented by connections where connections[i] = [ai, bi] represents a road from city ai to city bi.
+
+    This year, there will be a big event in the capital (city 0), and many people want to travel to this city.
+
+    Your task consists of reorienting some roads such that each city can visit the city 0. Return the minimum number of edges changed.
+
+    It's guaranteed that each city can reach city 0 after reorder.
+
+    Example 1:
+    -----------
+    Input: n = 6, connections = [[0,1],[1,3],[2,3],[4,0],[4,5]]
+    Output: 3
+    Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+
+    Example 2:
+    -------------
+    Input: n = 5, connections = [[1,0],[1,2],[3,2],[3,4]]
+    Output: 2
+    Explanation: Change the direction of edges show in red such that each node can reach the node 0 (capital).
+
+    Example 3:
+    ------------
+    Input: n = 3, connections = [[1,0],[2,0]]
+    Output: 0
+
+    Constraints:
+    -------------
+    2 <= n <= 5 * 104
+    connections.length == n - 1
+    connections[i].length == 2
+    0 <= ai, bi <= n - 1
+    ai != bi
+    """
+
+    edges = {(a, b) for a, b in connections}
+    neighbors = {city: [] for city in range(n)}
+    visited = set()
+    count = 0
+
+    for a, b in connections:
+        neighbors[a].append(b)
+        neighbors[b].append(a)
+
+    def dfs(city):
+        nonlocal edges, neighbors, visited, count
+
+        for neighbor in neighbors[city]:
+            if neighbor in visited:
+                continue
+
+            if (neighbor, city) not in edges:
+                count += 1
+
+            visited.add(neighbor)
+            dfs(neighbor)
+
+    visited.add(0)
+    dfs(0)
+    return count
