99a0406b4d5315f72fd064eebf9f3e5a6194bf05
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-15 14:19:18 -0400

    product_of_array_except_self_238

--- array.py
+++ array.py
@@ -79,3 +79,42 @@ def find_the_highest_altitude_1732(gain):
     for i in gain:
         res.append(res[-1]+i)
     return max(res)
+
+
+def product_of_array_except_self_238(nums):
+    """
+    Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+
+    You must write an algorithm that runs in O(n) time and without using the division operation.
+
+    Example 1:
+    ----------
+    Input: nums = [1,2,3,4]
+    Output: [24,12,8,6]
+
+    Example 2:
+    ------------
+    Input: nums = [-1,1,0,-3,3]
+    Output: [0,0,9,0,0]
+
+    Constraints:
+    -------------
+    2 <= nums.length <= 105
+    -30 <= nums[i] <= 30
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+    """
+
+    n = len(nums)
+
+    left = [1] * n
+    right = [1] * n
+
+    for i in range(1, n):  # not calculating the first index val
+        left[i] = left[i - 1] * nums[i - 1]
+
+    for i in range(n - 2, -1, -1):  # not calculating the kast index val
+        right[i] = right[i + 1] * nums[i + 1]
+
+    return [left[i] * right[i] for i in range(n)]
