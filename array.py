46236c6322ee98f45e96ef3cee51180c1f485d02
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-16 13:43:33 -0400

    two_sum_1

--- array.py
+++ array.py
@@ -118,3 +118,44 @@ def product_of_array_except_self_238(nums):
         right[i] = right[i + 1] * nums[i + 1]
 
     return [left[i] * right[i] for i in range(n)]
+
+
+def two_sum_1(nums, target):
+    """
+    Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+    You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+    You can return the answer in any order.
+
+    Example 1:
+    -----------
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+    Example 2:
+    ------------
+    Input: nums = [3,2,4], target = 6
+    Output: [1,2]
+
+    Example 3:
+    -------------
+    Input: nums = [3,3], target = 6
+    Output: [0,1]
+
+    Constraints:
+    -------------
+    2 <= nums.length <= 104
+    -109 <= nums[i] <= 109
+    -109 <= target <= 109
+    Only one valid answer exists.
+    """
+
+    memo = {}
+    for key, val in enumerate(nums):
+        res = target - val
+        if res in memo:
+            return [memo[res], key]
+        memo[val] = key
+    return []
