26098179d0e35a89c51b22925c0e3188e613a452
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-17 17:16:15 -0400

    maximum_product_subarray_152

--- array.py
+++ array.py
@@ -272,3 +272,45 @@ def maximum_subarray_53(nums):
             curr_sum = 0
 
     return max_sum
+
+
+def maximum_product_subarray_152(nums):
+    """
+    Given an integer array nums, find a subarray that has the largest product, and return the product.
+    The test cases are generated so that the answer will fit in a 32-bit integer.
+
+    Example 1:
+    -----------
+    Input: nums = [2,3,-2,4]
+    Output: 6
+    Explanation: [2,3] has the largest product 6.
+
+    Example 2:
+    -----------
+    Input: nums = [-2,0,-1]
+    Output: 0
+    Explanation: The result cannot be 2, because [-2,-1] is not a subarray.
+
+    Constraints:
+    -------------
+    1 <= nums.length <= 2 * 104
+    -10 <= nums[i] <= 10
+    The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+    """
+    res = max(nums)
+    cmax = 1
+    cmin = 1
+
+    for n in nums:
+
+        if n == 0:
+            cmax = 1
+            cmin = 1
+            continue
+
+        vals = (n, n * cmax, n * cmin)
+        cmax = max(vals)
+        cmin = min(vals)
+
+        res = max(res, cmax)
+    return res
