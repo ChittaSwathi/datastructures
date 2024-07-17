7ccc24cf7f8486570f3e20157e0f963e4131ac30
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-17 12:27:35 -0400

    maximum_subarray_53

--- array.py
+++ array.py
@@ -229,3 +229,46 @@ def contains_duplicate_217(nums):
         else:
             memo.add(i)
     return False
+
+
+def maximum_subarray_53(nums):
+    """
+    Given an integer array nums, find the subarray with the largest sum, and return its sum.
+
+    Example 1:
+    ------------
+    Input: nums = [-2,1,-3,4,-1,2,1,-5,4]
+    Output: 6
+    Explanation: The subarray [4,-1,2,1] has the largest sum 6.
+
+    Example 2:
+    -------------
+    Input: nums = [1]
+    Output: 1
+    Explanation: The subarray [1] has the largest sum 1.
+
+    Example 3:
+    --------------
+    Input: nums = [5,4,-1,7,8]
+    Output: 23
+    Explanation: The subarray [5,4,-1,7,8] has the largest sum 23.
+
+    Constraints:
+    --------------
+    1 <= nums.length <= 105
+    -104 <= nums[i] <= 104
+    """
+
+    max_sum = float("-inf")
+    curr_sum = 0
+
+    for num in nums:
+        curr_sum += num
+
+        if curr_sum > max_sum:
+            max_sum = curr_sum
+
+        if curr_sum < 0:
+            curr_sum = 0
+
+    return max_sum
