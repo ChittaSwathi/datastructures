3c0367e081d0e3140110805ab32c94a0ce937b55
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-19 23:45:46 -0400

    arrays

--- array.py
+++ array.py
@@ -409,3 +409,242 @@ def find_minimum_in_rotated_sorted_array_153(nums):
             r -= 1
 
     return res
+
+def merge_strings_alternately_1768(word1, word2):
+    """
+    You are given two strings word1 and word2. Merge the strings by adding letters in alternating order, starting with word1. If a string is longer than the other, append the additional letters onto the end of the merged string.
+
+    Return the merged string.
+
+    Example 1:
+    ------------
+    Input: word1 = "abc", word2 = "pqr"
+    Output: "apbqcr"
+    Explanation: The merged string will be merged as so:
+    word1:  a   b   c
+    word2:    p   q   r
+    merged: a p b q c r
+
+    Example 2:
+    -----------
+    Input: word1 = "ab", word2 = "pqrs"
+    Output: "apbqrs"
+    Explanation: Notice that as word2 is longer, "rs" is appended to the end.
+    word1:  a   b
+    word2:    p   q   r   s
+    merged: a p b q   r   s
+
+    Example 3:
+    -----------
+    Input: word1 = "abcd", word2 = "pq"
+    Output: "apbqcd"
+    Explanation: Notice that as word1 is longer, "cd" is appended to the end.
+    word1:  a   b   c   d
+    word2:    p   q
+    merged: a p b q c   d
+
+    Constraints:
+    -------------
+    1 <= word1.length, word2.length <= 100
+    word1 and word2 consist of lowercase English letters.
+    """
+    m = min([len(word1), len(word2)])
+    res = ''
+    for i in range(m):
+        res += word1[i] + word2[i]
+    res += word1[m:]
+    res += word2[m:]
+    return res
+
+def greatest_common_divisor_of_strings_1071(str1, str2):
+    """For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t (i.e., t is concatenated with itself one or more times).
+
+    Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+    
+    Example 1:
+    ------------
+    Input: str1 = "ABCABC", str2 = "ABC"
+    Output: "ABC"
+    
+    Example 2:
+    -----------
+    Input: str1 = "ABABAB", str2 = "ABAB"
+    Output: "AB"
+    
+    Example 3:
+    -----------
+    Input: str1 = "LEET", str2 = "CODE"
+    Output: ""
+     
+    Constraints:
+    --------------
+    1 <= str1.length, str2.length <= 1000
+    str1 and str2 consist of English uppercase letters.
+    """
+    len1, len2 = len(str1), len(str2)
+
+    for i in range(min(len1, len2), 0, -1):
+        s1, s2 = len1//i, len2//i
+        if str1[:i] * s1 == str1 and str1[:i] * s2 == str2:
+            return str1[:i]
+
+    return ""
+
+def kids_with_the_greatest_number_of_candies_1431(candies, extraCandies):
+    """
+    There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
+
+    Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
+
+    Note that multiple kids can have the greatest number of candies.
+
+    Example 1:
+    ------------
+    Input: candies = [2,3,5,1,3], extraCandies = 3
+    Output: [true,true,true,false,true]
+    Explanation: If you give all extraCandies to:
+    - Kid 1, they will have 2 + 3 = 5 candies, which is the greatest among the kids.
+    - Kid 2, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+    - Kid 3, they will have 5 + 3 = 8 candies, which is the greatest among the kids.
+    - Kid 4, they will have 1 + 3 = 4 candies, which is not the greatest among the kids.
+    - Kid 5, they will have 3 + 3 = 6 candies, which is the greatest among the kids.
+
+    Example 2:
+    -------------
+    Input: candies = [4,2,1,1,2], extraCandies = 1
+    Output: [true,false,false,false,false]
+    Explanation: There is only 1 extra candy.
+    Kid 1 will always have the greatest number of candies, even if a different kid is given the extra candy.
+
+    Example 3:
+    ------------
+    Input: candies = [12,1,12], extraCandies = 10
+    Output: [true,false,true]
+
+    Constraints:
+    -------------
+    n == candies.length
+    2 <= n <= 100
+    1 <= candies[i] <= 100
+    1 <= extraCandies <= 50
+    """
+    max_candy = max(candies)
+    for i in range(len(candies)):
+        if candies[i] + extraCandies >= max_candy:
+            candies[i] = True
+        else:
+            candies[i] = False
+    return candies
+
+
+def can_place_flowers_605(flowerbed, n):
+    """
+    You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+
+    Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+
+
+
+    Example 1:
+    ------------
+    Input: flowerbed = [1,0,0,0,1], n = 1
+    Output: true
+
+    Example 2:
+    -----------
+    Input: flowerbed = [1,0,0,0,1], n = 2
+    Output: false
+
+    Constraints:
+    ------------
+    1 <= flowerbed.length <= 2 * 104
+    flowerbed[i] is 0 or 1.
+    There are no two adjacent flowers in flowerbed.
+    0 <= n <= flowerbed.length
+    """
+    flowerbed = [0] + flowerbed + [0]
+    count = 0
+    for i in range(1, len(flowerbed) - 1):
+        if flowerbed[i - 1] == 0 and flowerbed[i] == 0 and flowerbed[i + 1] == 0:
+            count += 1
+            flowerbed[i] = 1
+    print('flowebed', flowerbed)
+    if count >= n:
+        return True
+    else:
+        return False
+
+
+def reverse_vowels_of_a_string(s):
+    """
+    Given a string s, reverse only all the vowels in the string and return it.
+
+    The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both lower and upper cases, more than once.
+
+    Example 1:
+    -----------
+    Input: s = "hello"
+    Output: "holle"
+
+    Example 2:
+    -----------
+    Input: s = "leetcode"
+    Output: "leotcede"
+
+    Constraints:
+    ------------
+    1 <= s.length <= 3 * 105
+    s consist of printable ASCII characters.
+    """
+    vowels = 'aeiouAEIOU'
+    given = list(s)
+    left, right = 0, len(s) - 1
+
+    while left < right:
+        if given[left] in vowels and given[right] in vowels:
+            given[left], given[right] = given[right], given[left]
+            left += 1
+            right -= 1
+        elif given[right] not in vowels:
+            right -= 1
+        elif given[left] not in vowels:
+            left += 1
+    return ''.join(given)
+
+
+def reverse_words_in_a_string_151(s):
+    """Given an input string s, reverse the order of the words.
+
+    A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+
+    Return a string of the words in reverse order concatenated by a single space.
+
+    Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+    Example 1:
+    ----------
+    Input: s = "the sky is blue"
+    Output: "blue is sky the"
+
+    Example 2:
+    -----------
+    Input: s = "  hello world  "
+    Output: "world hello"
+    Explanation: Your reversed string should not contain leading or trailing spaces.
+
+    Example 3:
+    -----------
+    Input: s = "a good   example"
+    Output: "example good a"
+    Explanation: You need to reduce multiple spaces between two words to a single space in the reversed string.
+
+    Constraints:
+    ------------
+    1 <= s.length <= 104
+    s contains English letters (upper-case and lower-case), digits, and spaces ' '.
+    There is at least one word in s.
+    """
+    revv = s.split(' ')[::-1]
+    return ' '.join([i.strip() for i in revv if i not in (' ')])
+
+
