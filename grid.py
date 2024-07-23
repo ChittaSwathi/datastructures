3834be508b3be8381545f78e1bd46ceac44bb9b4
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-23 18:11:06 -0400

    number_of_islands_200

--- grid.py
+++ grid.py
@@ -202,3 +202,55 @@ def nearest_exit_from_entrance_in_maze_1926(maze, entrance):
     return -1
 
 
+def number_of_islands_200(grid):
+    """
+    Given an m x n 2D binary grid, which represents a map of '1's (land) and '0's (water), return the number of islands.
+
+    An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
+
+    Example 1:
+    ------------
+    Input: grid = [
+      ["1","1","1","1","0"],
+      ["1","1","0","1","0"],
+      ["1","1","0","0","0"],
+      ["0","0","0","0","0"]
+    ]
+    Output: 1
+
+    Example 2:
+    ------------
+    Input: grid = [
+      ["1","1","0","0","0"],
+      ["1","1","0","0","0"],
+      ["0","0","1","0","0"],
+      ["0","0","0","1","1"]
+    ]
+    Output: 3
+
+    Constraints:
+    --------------
+    m == grid.length
+    n == grid[i].length
+    1 <= m, n <= 300
+    grid[i][j] is '0' or '1'.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    islands = 0
+
+    def dfs(r, c):
+        nonlocal islands, rows, cols, grid
+        if r >= rows or r < 0 or c < 0 or c >= cols or grid[r][c] != '1':
+            return
+
+        grid[r][c] = '*'
+        for dr, dc in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+            dfs(r + dr, c + dc)
+
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == '1':
+                islands += 1
+                dfs(row, col)
+    return islands
