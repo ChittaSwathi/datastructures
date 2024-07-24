c6e5ab9026d9259c5eb817cbd397eb1341c61683
Swathi Chitta <chittaswathi98@gmail.com>
2024-07-24 14:23:22 -0400

    climbing_stairs_70

--- dp.py
+++ dp.py
@@ -39,6 +39,41 @@ def n_th_tribonacci_number_1137(n):
     return fib(n)
 
 
+def climbing_stairs_70(n):
+    """You are climbing a staircase. It takes n steps to reach the top.
+
+    Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+    Example 1:
+    ----------
+    Input: n = 2
+    Output: 2
+    Explanation: There are two ways to climb to the top.
+    1. 1 step + 1 step
+    2. 2 steps
+
+    Example 2:
+    ----------
+    Input: n = 3
+    Output: 3
+    Explanation: There are three ways to climb to the top.
+    1. 1 step + 1 step + 1 step
+    2. 1 step + 2 steps
+    3. 2 steps + 1 step
+
+    Constraints:
+    ------------
+    1 <= n <= 45"""
+    def climb(n, memo):
+        if n in memo:
+            return memo[n]
+        memo[n] = climb(n - 1, memo) + climb(n - 2, memo)
+        return memo[n]
+
+    memo = {0: 0, 1: 1, 2: 2}
+    return climb(n, memo)
+
+
 def min_cost_climbing_stairs_746(cost):
     """You are given an integer array cost where cost[i] is the cost of ith step on a staircase. Once you pay the cost, you can either climb one or two steps.
 
