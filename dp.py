f294aac4795c06885bbacc89f19ba2b33fdaf7c0
Swathi Chitta <chittaswathi98@gmail.com>
2024-08-08 13:32:04 -0400

    decode_ways_91

--- dp.py
+++ dp.py
@@ -158,3 +158,84 @@ def house_robber_198(nums):
 
     return memo[-1]
 
+
+
+def decode_ways_91(s):
+    """
+    You have intercepted a secret message encoded as a string of numbers. The message is decoded via the following mapping:
+    "1" -> 'A'
+    "2" -> 'B'
+    ...
+    "25" -> 'Y'
+    "26" -> 'Z'
+
+    However, while decoding the message, you realize that there are many different ways you can decode the message because some codes are contained in other codes ("2" and "5" vs "25").
+    For example, "11106" can be decoded into:
+    "AAJF" with the grouping (1, 1, 10, 6)
+    "KJF" with the grouping (11, 10, 6)
+    The grouping (1, 11, 06) is invalid because "06" is not a valid code (only "6" is valid).
+    Note: there may be strings that are impossible to decode.
+
+    Given a string s containing only digits, return the number of ways to decode it. If the entire string cannot be decoded in any valid way, return 0.
+    The test cases are generated so that the answer fits in a 32-bit integer.
+
+    Example 1:
+    -----------
+    Input: s = "12"
+    Output: 2
+    Explanation:
+    "12" could be decoded as "AB" (1 2) or "L" (12).
+
+    Example 2:
+    ------------
+    Input: s = "226"
+    Output: 3
+    Explanation:
+    "226" could be decoded as "BZ" (2 26), "VF" (22 6), or "BBF" (2 2 6).
+
+    Example 3:
+    -----------
+    Input: s = "06"
+    Output: 0
+    Explanation:
+    "06" cannot be mapped to "F" because of the leading zero ("6" is different from "06"). In this case, the string is not a valid encoding, so return 0.
+
+    Constraints:
+    --------------
+    1 <= s.length <= 100
+    s contains only digits and may contain leading zero(s).
+    """
+
+    # only dp solution
+    dp = [0] * (len(s) + 1)
+    # dp[i] denotes decodings till s[i-1]; so dp[i] is dependent on dp[i-1] and dp[i-2]
+    dp[0] = 1
+    dp[1] = 1
+
+    if s[0] == '0': return 0
+
+    for i in range(2, len(s) + 1):
+        if 1 <= int(s[i - 1]) <= 9: #checking current single char is between 1 and 9
+            dp[i] += dp[i - 1]
+        if 10 <= int(s[i - 2] + s[i - 1]) <= 26: # checking if two digit val lies between 10 and 26
+            dp[i] += dp[i - 2]
+
+    return dp[-1]
+
+    # dp with dfs solution
+    dp = {len(s): 1}
+
+    def dfs(i):
+        if i in dp:
+            return dp[i]
+        if s[i] == '0':
+            return 0
+
+        res = dfs(i + 1)
+        if i+1 < len(s) and 10<= int(s[i]+s[i+1]) <= 26:
+            # checking if two digit val lies between 10 and 26
+            res += dfs(i + 2)
+        dp[i] = res
+        return res
+
+    return dfs(0)
