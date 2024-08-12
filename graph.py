3365a7f8daac95508d95b6d26ba4b860384d04df
Swathi Chitta <swathi@macbook-air.mynetworksettings.com>
2024-08-12 18:32:59 -0400

    course_schedule_207

--- graph.py
+++ graph.py
@@ -2,6 +2,61 @@ from collections import deque
 import math
 from typing import Optional
 
+def course_schedule_207(numCourses, prerequisites):
+    """
+    There are a total of numCourses courses you have to take, labeled from 0 to numCourses - 1. You are given an array prerequisites where prerequisites[i] = [ai, bi] indicates that you must take course bi first if you want to take course ai.
+    For example, the pair [0, 1], indicates that to take course 0 you have to first take course 1.
+    Return true if you can finish all courses. Otherwise, return false.
+
+    Example 1:
+    ----------
+    Input: numCourses = 2, prerequisites = [[1,0]]
+    Output: true
+    Explanation: There are a total of 2 courses to take. 
+    To take course 1 you should have finished course 0. So it is possible.
+    
+    Example 2:
+    ------------
+    Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+    Output: false
+    Explanation: There are a total of 2 courses to take. 
+    To take course 1 you should have finished course 0, and to take course 0 you should also have finished course 1. So it is impossible.
+    
+    Constraints:
+    --------------
+    1 <= numCourses <= 2000
+    0 <= prerequisites.length <= 5000
+    prerequisites[i].length == 2
+    0 <= ai, bi < numCourses
+    All the pairs prerequisites[i] are unique.
+    """
+
+    #dfs - return false for every applicable scenario
+    visited = set()
+    course_lookup = {i:[] for i in range(numCourses)}
+    for i in prerequisites:
+        course_lookup[i[0]].append(i[1])
+
+    def dfs(course, visited):
+        if course in visited: #to check loop
+            return False
+        if course_lookup[course] == []: #no dependency courses
+            return True
+        
+        visited.add(course)
+        for c in course_lookup[course]:
+            if not dfs(c, visited): 
+                return False
+        visited.remove(course)
+        course_lookup[course] = []
+        return True
+    
+    for c in range(numCourses):
+        if not dfs(c, visited):
+            return False
+    
+    return True
+
 
 def clone_graph_133(node):
     """Given a reference of a node in a connected undirected graph.
